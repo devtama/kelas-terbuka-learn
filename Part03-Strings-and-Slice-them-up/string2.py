3995279216db0b51e2191fa9d5bd9292e467c886
devtama <48782708+devtama@users.noreply.github.com>
2019-03-22 13:20:59 +0700

    String

---
+++ Part03-Strings-and-Slice-them-up/string2.py
@@ -0,0 +1,5 @@
+text = "kue pukis"
+
+a = text[2:]
+
+print(a)
---
+++ Part03-Strings-and-Slice-them-up/strings.py
@@ -0,0 +1,23 @@
+text1 = 'jalan - jalan di hari minggu'
+
+text2 = 'jalan - jalan di hari jum\'at'
+
+text3 = "jalan - jalan di hari jum'at"
+
+text4 = 'mahmuy berkata, "kemaren kemana bro?"'
+
+text5 = "hobloh menjawab, \"Jalan-jalan bro\""
+
+text6 = 'mahmuy: "kemaren kemana bro?" \nhobloh: "Jalan-jalan bro"'
+
+text7 = """
+mahmuy: "kemaren kemana bro?"
+hobloh: "Jalan-jalan bro"
+mahmuy: "jalan jalan kemana?"
+hobloh: "ke mang uing bro"
+"""
+
+text8 = r'C:\nyoto' # raw string
+
+print(text8)
+print(10*"wk")
