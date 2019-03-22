703c8d9d4b380b5b8e573d711aef3a2ebe5be1f9
Tama Pratama <pratamautama163@gmail.com>
2019-03-22 18:49:42 +0700

    Range-Break

---
+++ Part07-For-Else-Range-Break/Range-Break.py
@@ -0,0 +1,11 @@
+cari_angka = 43
+
+for i in range(0,41):
+    print(i)
+
+    if i is cari_angka:
+        print('angka ditemukan',i)
+        break
+else:
+    print('angka',cari_angka,'tidak ditemukan')
+
