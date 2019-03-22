8045f6a6ab8542a8150774ac0681f18cfdfbff89
devtama <48782708+devtama@users.noreply.github.com>
2019-03-22 11:26:27 +0700

    Penjumlahan

---
+++ Part02-Number-and-Math/Number-and-Math.py
@@ -0,0 +1,31 @@
+a = 4
+b = 3
+c = 4.0
+
+# Penjumlahan
+d = a + b
+print(f"penjumlahan {a} + {b} = {d}")
+
+# Pengurangan
+d = a - b
+print(f"Pengurangan {a} - {b} = {d}")
+
+# Pembagian
+d = a / b
+print(f"Pembagian {a} : {b} = {d}")
+
+# Perkalian
+d = a * b
+print(f"Perkalian {a} x {b} = {d}")
+
+# Modulus
+d = a % b
+print(f"Sisa dari Pembagian {a} : {b} = {d}")
+
+# Bulatkan
+d = a // b
+print(f"Pembagian {a} : {b} dibulatkan = {d}")
+
+# Pangakat
+d = a**3
+print(f"Hasil dari {a} pangkat 3 adalah {d}")
