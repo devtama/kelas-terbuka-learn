edca0ad438c9e253858e0c24cc147b72903b1f11
Tama Pratama <pratamautama163@gmail.com>
2019-03-22 19:01:14 +0700

    Continue and Pass

---
+++ Part08-Continue-Pass/continue-pass.py
@@ -0,0 +1,18 @@
+for i in range(1,10):
+
+    if i is 6:
+        print("ada di",6)
+        #break # untuk mengakhiri for
+        #continue # untuk melanjutkan ke step berikutnya
+        #pass # untuk kata kunci kosong untuk konstruksi fungsi/loop
+        print("maju")
+
+    print("nilai saat ini adalah:",i)
+
+
+else:
+    print("akhir loop")
+
+print("outside")
+
+## pass ini bisa dipake untuk dummy looping, if, fungsi, dll
