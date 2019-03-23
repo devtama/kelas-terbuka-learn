4e8c0a0723f3559cccd0a314b22be8094f9295f9
Tama Pratama <pratamautama163@gmail.com>
2019-03-23 18:35:48 +0700

    Lambda Function

---
+++ Part13-Lambda-Funtion/lambda.py
@@ -0,0 +1,11 @@
+def jumlah(a,b):
+    c = a+b
+    return c
+
+hasil = jumlah(4,3)
+
+# Membuat anonymous function dengan lambda
+kali = lambda x,y: x*y
+hasil = kali(9,8)
+
+print(hasil)
---
+++ Part13-Lambda-Funtion/latihan.py
@@ -0,0 +1,6 @@
+import math
+
+luas_lingkaran = lambda j : math.pi * (j**2)
+
+hasil = luas_lingkaran(10)
+print(hasil)
